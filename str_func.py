3ea6b11379a63447e81dde818d44fe68e0f7591f
nadyash83 <bezborodova@pkkb1.ru>
2023-12-05 19:15:14 +1000

    add capital_letters_first

--- str_func.py
+++ str_func.py
@@ -1,3 +1,16 @@
 def capital_letters(word):
+    """
+    Функция поднимает все слово в верхний регистр
+    :param word: word
+    :return: WORD
+    """
     return word.append()
+
+def capital_letters_first(word):
+    """
+    Функция делает первую букву заглавной
+    :param word: word
+    :return: Word
+    """
+    return word.title()
 
